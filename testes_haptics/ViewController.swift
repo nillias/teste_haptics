6c90f02fe9f4dfad2d70b26a571907e6ccd82f0f
Nillia Sousa <nilliasusa@gmail.com>
2022-09-14 10:41:28 -0300

    Initial Commit

---
+++ testes_haptics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testes_haptics
+//
+//  Created by Nillia Sousa on 14/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
